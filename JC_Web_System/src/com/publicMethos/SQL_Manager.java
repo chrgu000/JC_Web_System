bccadf4d7b564517356d8aff58b93ce35f6fd79b
Administrator <Administrator@192.168.1.101>
2017-02-16 00:21:47 +0800

    0.21

--- JC_Web_System/src/com/publicMethos/SQL_Manager.java
+++ JC_Web_System/src/com/publicMethos/SQL_Manager.java
@@ -3,7 +3,6 @@ package com.publicMethos;
 import java.util.List;
 
 import org.hibernate.Query;
-import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -13,7 +12,11 @@ public final class SQL_Manager {
 
 	//执行原生sql查询语句，并返回对象列表
 	public static List QuerySQL(String sql)  throws Exception{
-		 SQLQuery query  =  sessionFactory.getCurrentSession().createSQLQuery(hql); //返回对象
+		Configuration config = new Configuration().configure();
+		SessionFactory sf     = config.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction ts = session.beginTransaction();
+		Query query = session.createSQLQuery(sql); //返回对象
 		return query.list(); 
 		
 	}
